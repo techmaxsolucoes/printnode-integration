8c4fe672d6ef72c1a2d109812bc16dffefb85b47
Maxwell Morais <max.morais.dmm@gmail.com>
2017-09-08 10:41:13 -0400

    Fixed patch for instances that dont have printnode before

--- printnode_integration/patches/deprecate_print_on_submit.py
+++ printnode_integration/patches/deprecate_print_on_submit.py
@@ -5,5 +5,10 @@ from __future__ import unicode_literals
 import frappe
 
 def execute():
-    frappe.db.sql('UPDATE `tabPrint Node Action` SET `print_on` = "Submit" WHERE `print_on_submit`=1;')
-    frappe.db.commit()
+    if frappe.db.sql('''
+	SELECT * FROM information_schema.COLUMNS 
+	WHERE TABLE_SCHEMA=%s
+		AND TABLE_NAME="tabPrint Node Action"
+		AND COLUMN_NAME = "print_on_submit"''', (frappe.get_conf().db_name,)):
+	    frappe.db.sql('UPDATE `tabPrint Node Action` SET `print_on` = "Submit" WHERE `print_on_submit`=1;')
+	    frappe.db.commit()
