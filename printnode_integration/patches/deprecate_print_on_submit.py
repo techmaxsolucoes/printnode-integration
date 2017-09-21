30dc31bd755096a72e3a93c0d537f63a84bedb6d
Maxwell Morais <max.morais.dmm@gmail.com>
2017-09-21 03:58:42 +0000

    Fix patch for legacy frappe version

--- printnode_integration/patches/deprecate_print_on_submit.py
+++ printnode_integration/patches/deprecate_print_on_submit.py
@@ -5,10 +5,16 @@ from __future__ import unicode_literals
 import frappe
 
 def execute():
+	if hasattr(frappe, 'get_site_config'):
+		# for legacy frappe versions
+		config = frappe.get_site_config()
+	else:
+		config = frappe.get_conf()
+
     if frappe.db.sql('''
 	SELECT * FROM information_schema.COLUMNS 
 	WHERE TABLE_SCHEMA=%s
 		AND TABLE_NAME="tabPrint Node Action"
-		AND COLUMN_NAME = "print_on_submit"''', (frappe.get_conf().db_name,)):
+		AND COLUMN_NAME = "print_on_submit"''', (config.db_name,)):
 	    frappe.db.sql('UPDATE `tabPrint Node Action` SET `print_on` = "Submit" WHERE `print_on_submit`=1;')
 	    frappe.db.commit()
