9bb404035916c6d28b292043cb6c0bc8fc7668bb
Maxwell Morais <max.morais.dmm@gmail.com>
2019-01-21 15:31:52 +0000

    Allow batch print on backend

--- printnode_integration/events.py
+++ printnode_integration/events.py
@@ -3,35 +3,46 @@
 from __future__ import unicode_literals
 import json
 import frappe
+from frappe.utils.background_jobs import enqueue
 from . import api
 
-def print_via_printnode( doc, event ):
-	ignore_flags = False
-	if isinstance( doc, basestring ):
-		doc = json.loads( doc, object_pairs_hook=frappe._dict )
-		ignore_flags = True
+def print_via_printnode( doctype, docname, docevent):
+	if not frappe.db.exists(doctype, docname):
+		enqueue('printnode_integration.events.print_via_printnode', enqueue_after_commit=False, doctype=doctype, docname=docname, docevent=docevent)
+
+	doc = frappe.get_doc(doctype, docname)
+	ignore_flags = True
 	
 	if not ignore_flags:
 		if doc.flags.on_import or doc.flags.ignore_print:
 			return
 
-	if not frappe.db.exists("Print Node Action", {"dt": doc.doctype, "print_on": event}):
+	if not frappe.db.exists("Print Node Action", {"dt": doc.doctype, "print_on": docevent}):
 		return
 
-	for d in frappe.get_list("Print Node Action", ["name", "ensure_single_print"], {"dt": doc.doctype, "print_on": event}):
-		if event == "Update" and d.ensure_single_print and frappe.db.exists("Print Job", d.name):
+	for d in frappe.get_list("Print Node Action", ["name", "ensure_single_print", "allow_inline_batch", "batch_field"], {"dt": doc.doctype, "print_on": docevent}):
+		if docevent == "Update" and d.ensure_single_print and frappe.db.exists("Print Job", d.name):
 			continue
-		api.print_via_printnode(d.name, doctype=doc.doctype, docname=doc.name)
-
+		if not row.allow_inline_batch:
+			api.print_via_printnode(d.name, doctype=doc.doctype, docname=doc.name)
+		else:
+			if '.' in row.batch_field:
+				table_field = row.batch_field.split('.')[0]
+				reference_list = doc.get(table_field)
+			else:
+				reference_list = [doc]
+			inline_field = row.batch_field.split('.')[-1]
+			api.batch_print_via_printnode(d.name, map(lambda d: d.get(inline_field), reference_list))
 
 def after_insert( doc, handler=None ):
-	print_via_printnode( doc, "Insert" )
+	enqueue('printnode_integration.events.print_via_printnode', enqueue_after_commit=True, doctype=doc.doctype, docname=doc.name, docevent='Insert', now=True)
 
 def on_update( doc, handler=None ):
-	print_via_printnode( doc, "Update" )
+	enqueue('printnode_integration.events.print_via_printnode', enqueue_after_commit=True, doctype=doc.doctype, docname=doc.name, docevent='Update', now=True)
 	
 def on_submit(doc, handler=None):
-	print_via_printnode( doc, "Submit" )
+	print((doc.doctype, doc.name, 'Submit'))
+	enqueue('printnode_integration.events.print_via_printnode', enqueue_after_commit=True, doctype=doc.doctype, docname=doc.name, docevent='Submit', now=True)
 
 def on_trash(doc, handler=None):
 	settings = frappe.get_doc('Print Node Settings', 'Print Node Settings')
